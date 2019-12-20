797def9f2d97d85d283d6cd6effb7ac88fe5e13f
Aaron Thomas <acthomas83@gmail.com>
2019-12-20 01:24:24 -0500

    final changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var resultEl = document.querySelector('.tetra');
+var resultEl = document.getElementById('password');
 var lengthEl = document.getElementById('length');
 var uppercaseEl = document.getElementById('uppercase');
 var lowercaseEl = document.getElementById('lowercase');
@@ -6,7 +6,7 @@ var numbersEl = document.getElementById('numbers');
 var symbolsEl = document.getElementById('symbols');
 var generateEl = document.getElementById('generate');
 var clipboard = document.getElementById('copy');
--
+
 var randomFunc = {
 	lower: getRandomLower,
 	upper: getRandomUpper,
@@ -14,41 +14,44 @@ var randomFunc = {
 	symbol: getRandomSymbol,
 }
 
+console.log(uppercaseEl)
 
+// var lowercase = ["a", "b", "c"];
+// var uppercase = ["D", "E", "F"];
+// var symbols = ["!", "@", "#"];
 
+// var random = [];
 
 // make an array of lowercase letters
-let uppercase = ['U', 'X']
+// let uppercase = ['U', 'X']
 // make an array of uppercase letters
 // make an array of symbols
 // create an empty array that will hold your random letters
 // for loop to go through array
-  for(var i=0; i < uppercase.length; i++){
-    var randomNum = Math.floor(Math.random() * 27)
-    var randomLetter = uppercase[randomNum]
-    // save this variable by pushing it to an empty array
-    
-  }
-  
+
+
+
+// function getRandomUpper() {
+//   for(var i=0; i < uppercase.length; i++){
+//     var randomNum = Math.floor(Math.random() * 27)
+//     var randomLetter = uppercase[randomNum]
+//     // save this variable by pushing it to an empty array
+//     random.push(randomLetter)
+//     console.log(random)
+//   }
+// }
+
+//   function getRandomLower() {
+//     for(var i=0; i < lowercaseEl.length; i++){
+//       var randomNum = Math.floor(Math.random() * 27)
+//       var randomLetter = lowercaseEl[randomNum]
+//       random.push(randomLetter)
+
+//     }
+// }
+
   // you will have a list of random letters
   // which will be generated password
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 clipboard.addEventListener('click', function() {
@@ -76,11 +79,40 @@ generateEl.addEventListener('click', function() {
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
-	var generatedPassword = '';
-	var typesCount = lower + upper + number + symbol;
-	var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 	
 	// Doesn't have a selected type
 	if(typesCount === 0) {
 		return '';
-	}}
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
